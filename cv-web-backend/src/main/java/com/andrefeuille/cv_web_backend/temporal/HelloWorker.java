0b409d36b4d846fc652c5a0052674f8b5132df72
Andris Lapins <lapinsandris9@gmail.com>
2025-04-28 21:44:35 +0300

    Update temporal

--- cv-web-backend/src/main/java/com/andrefeuille/cv_web_backend/temporal/HelloWorker.java
+++ cv-web-backend/src/main/java/com/andrefeuille/cv_web_backend/temporal/HelloWorker.java
@@ -1,31 +1,23 @@
 package com.andrefeuille.cv_web_backend.temporal;
 
 import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
-// import org.springframework.beans.factory.annotation.Value;
-
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import com.andrefeuille.cv_web_backend.temporal.hello_activity.HelloActivityImpl;
 import com.andrefeuille.cv_web_backend.temporal.hello_workflow.HelloWorkflowImpl;
 
 import io.temporal.client.WorkflowClient;
-import io.temporal.serviceclient.WorkflowServiceStubs;
-import io.temporal.serviceclient.WorkflowServiceStubsOptions;
 import io.temporal.worker.Worker;
 import io.temporal.worker.WorkerFactory;
-import org.springframework.beans.factory.annotation.Value;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
 @Component
 @Slf4j
 @RequiredArgsConstructor
 public class HelloWorker {
-
-    @Value("${spring.temporal.connection.target}")
-    private String temporalTarget;
 
     private final WorkflowClient workflowClient;
 
@@ -40,8 +32,8 @@ public class HelloWorker {
             worker.registerWorkflowImplementationTypes(HelloWorkflowImpl.class);
             worker.registerActivitiesImplementations(new HelloActivityImpl());
 
-            log.info("🛠️ Temporal worker started and polling {}", HelloNames.HELLO_WORLD_TASK_QUEUE);
             factory.start();
+            log.info("🛠️ Temporal worker started and polling {}", HelloNames.HELLO_WORLD_TASK_QUEUE);
         }).start();
     }
 }
--- cv-web-backend/src/main/java/com/andrefeuille/cv_web_backend/temporal/TemporalConfig.java
+++ cv-web-backend/src/main/java/com/andrefeuille/cv_web_backend/temporal/TemporalConfig.java
@@ -1,35 +1,30 @@
 package com.andrefeuille.cv_web_backend.temporal;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import io.temporal.client.WorkflowClient;
 import io.temporal.serviceclient.WorkflowServiceStubs;
-import io.temporal.worker.Worker;
-import io.temporal.worker.WorkerFactory;
+import io.temporal.serviceclient.WorkflowServiceStubsOptions;
 
 @Configuration
 public class TemporalConfig {
 
+    @Value("${spring.temporal.connection.target}")
+    private String temporalTarget;
+
     @Bean
     public WorkflowServiceStubs workflowServiceStubs() {
-        return WorkflowServiceStubs.newLocalServiceStubs(); // you can adjust this
+        return WorkflowServiceStubs.newInstance(
+            WorkflowServiceStubsOptions.newBuilder()
+                .setTarget(temporalTarget)
+                .build()
+        );
     }
 
     @Bean
     public WorkflowClient workflowClient(WorkflowServiceStubs serviceStubs) {
         return WorkflowClient.newInstance(serviceStubs);
-    }
-
-    @Bean
-    public WorkerFactory workerFactory(WorkflowClient client) {
-        WorkerFactory factory = WorkerFactory.newInstance(client);
-        Worker worker = factory.newWorker("cv-web-task-queue");
-
-        worker.registerWorkflowImplementationTypes(ExampleWorkflowImpl.class);
-        worker.registerActivitiesImplementations(new ExampleActivitiesImpl());
-
-        factory.start();
-        return factory;
     }
 }
