4ffc27c9fcc7e5e6be90268b75cbb3f89e9a8db6
Andris Lapins <lapinsandris9@gmail.com>
2025-05-01 12:47:44 +0300

    Update Frontend backend api URL

--- cv-web-frontend/src/api/index.ts
+++ cv-web-frontend/src/api/index.ts
@@ -2,9 +2,9 @@ import { Configuration } from '../generated/configuration'
 import { JobControllerApi } from '../generated/apis/job-controller-api'
 import { EducationControllerApi } from '../generated/apis/education-controller-api'
 
-const configuration = new Configuration({
-  basePath: 'http://localhost:8080',
-})
+const basePath = 'http://localhost:8080'
+console.log(`📡 Connecting frontend to backend API at: ${basePath}`)
+const configuration = new Configuration({ basePath })
 
 export const jobApi = new JobControllerApi(configuration)
 export const educationApi = new EducationControllerApi(configuration)
